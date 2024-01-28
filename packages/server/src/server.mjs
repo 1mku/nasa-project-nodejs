096da156147579cc3ea763c7b5a90fd85ef7bd3c
Mykhailo Kushchov <mkuschov@gmail.com>
2024-01-27 20:15:39 -0500

    server: add mongoose, set up db connection

--- packages/server/src/server.mjs
+++ packages/server/src/server.mjs
@@ -1,18 +1,28 @@
 import http from 'http';
+import mongoose from 'mongoose';
 import app from './app.mjs';
+import { env } from 'node:process'
 
-import { loadPlanetsData } from './models/planets.model.mjs';
+const { MONGO_USERNAME, MONGO_PASSWORD, PORT = 8000 } = env;
 
-const PORT = process.env.PORT || 8000;
+
+const MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}`
+	+ '@nodedevcluster0.soi2yfw.mongodb.net/'
+	+ '?retryWrites=true&w=majority';
+mongoose.connection.once('connected', () => console.log('db:connected'))
+mongoose.connection.on('error', (err) => console.log('db:error', err))
 
 const server = http.createServer(app);
-
 async function startServer() {
-	await loadPlanetsData();
+	try {
+		await mongoose.connect(MONGO_URL)
+		server.listen(PORT, () => {
+			console.log(`Listening on port: ${PORT}`);
+		});
+	} catch (error) {
+		console.log('db:error', err);
+	}
 
-	server.listen(PORT, () => {
-		console.log(`Listening on port: ${PORT}`);
-	});
 }
 
 startServer();
